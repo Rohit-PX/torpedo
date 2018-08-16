13154f0f95df3ef245393d3ed415a53a82979e8b
Rohit-PX <rohit@portworx.com>
2018-08-16 14:16:21 -0700

    Change condition to != from >
    Signed-off-by: Rohit-PX <rohit@portworx.com>

--- vendor/github.com/portworx/sched-ops/k8s/k8s.go
+++ vendor/github.com/portworx/sched-ops/k8s/k8s.go
@@ -1122,7 +1122,7 @@ func (k *k8sOps) ValidateDeployment(deployment *apps_api.Deployment) error {
 		}
 
 		logrus.Infof("RK=> Checking UpdatedReplicas.")
-		if requiredReplicas > dep.Status.UpdatedReplicas {
+		if requiredReplicas != dep.Status.UpdatedReplicas {
 			return "", true, &ErrAppNotReady{
 				ID: dep.Name,
 				Cause: fmt.Sprintf("Expected replicas: %v Updated replicas: %v Current pods overview:\n%s",
